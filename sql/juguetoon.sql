364800a91a035635953780bdc7486ba9062e45b8
Mario Sumariva <mariosumd@gmail.com>
2015-11-26 20:50:09 +0100

    Resuelve conflicto de Guillermo en sql

--- sql/juguetoon.sql
+++ sql/juguetoon.sql
@@ -1,3 +1,4 @@
+
 create table usuarios (
     id          bigserial       constraint pk_socios primary key,
     numero      numeric(13)     not null constraint uq_socios_numero unique, 
@@ -9,3 +10,12 @@ create table usuarios (
 insert into socios(numero, nick, password, admin)
 values  (1000, 'juan', md5('juan'), true),
         (1001, 'maria', md5('maria'), false);
+
+create table articulos (
+    id bigserial constraint constraint pk_articulos primary key,
+    codigo char(13) not null constraint uq_articulos_codigo unique,
+    nombre varchar(50),
+    descripcion varchar(150),
+    precio numeric(6,2) not null
+);
+
