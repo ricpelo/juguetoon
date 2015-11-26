56afefd5f05038b9f548ce875d43909c4886a17f
Guillermo <guillermo.lopez@iesdonana.org>
2015-11-26 20:36:46 +0100

    SQL de usuarios basico, muy basico

---
+++ sql/juguetoon.sql
@@ -0,0 +1,11 @@
+create table usuarios (
+    id          bigserial       constraint pk_socios primary key,
+    numero      numeric(13)     not null constraint uq_socios_numero unique, 
+    nick        varchar(100)    not null,
+    password    char(32),
+    admin       bool           not null default false
+);
+
+insert into socios(numero, nick, password, admin)
+values  (1000, 'juan', md5('juan'), true),
+        (1001, 'maria', md5('maria'), false);
