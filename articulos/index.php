4c62e86c5f4c395d5a454eaeb36eae9041dc7357
Sergio Alcántara <sergio.alcantara.1992@gmail.com>
2015-11-27 19:01:59 +0100

    Finaliza index.php de articulos

--- articulos/index.php
+++ articulos/index.php
@@ -3,66 +3,42 @@
         <meta charset="utf-8" />
         <title>Juguetes</title>
     </head>
-    <body><?php
-
-        function conectar() {
-            return pg_pconnect("host=localhost dbname=datos user=usuario
-                        password=usuario");
-        }
+    <body>
+        <?php
+        //require 'auxiliar.php';
+        require '../comunes/auxiliar.php';
 
         $columnas = array(
             'codigo' => array(
-                'bonito' => 'Código',
+                'bonito'   => 'Código',
                 'criterio' => 'codigo',
-                'exacto' => TRUE,
-                'align' => 'center'
+                'exacto'   => TRUE,
+                'mostrar' => TRUE
             ),
             'nombre' => array(
-                'bonito' => 'Nombre',
-                'criterio' => 'nombre'
+                'bonito'   => 'Nombre',
+                'criterio' => 'nombre',
+                'mostrar' => TRUE
             ),
             'descripcion' => array(
-                'bonito' => 'Descripcion'
+                'bonito'   => 'Descripcion',
+                'criterio' => 'descripcion',
+                'mostrar' => TRUE
             ),
             'precio' => array(
-                'bonito' => 'Precio',
+                'bonito'   => 'Precio',
                 'criterio' => 'precio',
-                'exacto' => TRUE,
-                'align' => 'center'
+                'exacto'   => TRUE,
+                'mostrar' => TRUE
+            ),
+            'existencias' => array(
+                'bonito'   => 'Existencias',
+                'criterio' => 'existencias',
+                'exacto'   => TRUE,
+                'mostrar' => TRUE
             )
         );
-        ?>
-        <p>
-            <form action="index.php" method="get">
-                <label for="criterio">Buscar:</label>
-                <select name="columna"><?php
-                    foreach ($columnas as $v):
-                        if (isset($v['criterio'])):
-                            ?>
-                            <option value="<?= $v['criterio'] ?>" >
-                                <?= $v['bonito'] ?>
-                            </option><?php
-                        endif;
-                    endforeach;
-                    
-                    ?>
-                </select>
-                <input type="text" name="criterio" />
-                <input type="submit" value="Buscar">
-            </form>
-        </p>
-    <?php
-    $con = conectar();
-    ?>
-        <p><table border="1" style="margin:auto">
-            <thead><?php
-        $href = "index.php?";
-        foreach ($columnas as $k => $v):?>
-                <th>
-                    <?= $v['bonito']?>
-                </th><?php endforeach; ?>
-            <th colspan="2">Operaciones</th>
-        </thead>
-        </table></p>
-</body>
+
+        index($columnas, 'v_productos'); ?>
+    </body>
 </html>
