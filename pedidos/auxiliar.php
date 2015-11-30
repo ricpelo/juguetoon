1878fc33e133ecdc1e7d6b7b42edeed6aa1817c1
Vicente López <vicentejlopezballen@gmail.com>
2015-11-30 16:38:41 +0100

    Añado pedidos index con boton de añadir al carrito

---
+++ pedidos/auxiliar.php
@@ -0,0 +1,81 @@
+<?php     
+
+
+function index1($columnas, $vista) {
+                
+        extract(recoger_parametros($columnas));
+        
+        $params = compact('columnas', 'columna', 'criterio', 'orden', 'sentido');
+        
+        formulario_busqueda($params);
+        
+        $con = conectar();
+        
+        list($where, $pqp) = filtro($columnas, $columna, $criterio);
+        
+        $res = pg_query_params($con, "select * from $vista
+                                      where $where
+                                      order by $orden $sentido", $pqp);
+ 
+        $params['res'] = $res;
+        
+        generar_resultado1($params); ?>
+        <a href="insertar.php"><input type="button" value="Insertar" /></a><?php
+    }
+
+function generar_resultado1($params) {
+        
+        extract($params);
+        
+        if(pg_num_rows($res) > 0): ?>
+            <table border="1" style="margin: auto;">
+                <thead><?php
+                    $href = "index.php?";
+                                      
+                    foreach($columnas as $k => $v):
+                        if(isset($v['mostrar'])):
+                            $sufijo = sentido($k, $orden, $sentido); ?>
+                            <th>
+                                <a href="<?= "${href}orden=$k&sentido=$sufijo" ?>">
+                                    <input type="button" value="<?= $v['bonito'] ?>" <?= resaltar($k, $orden) ?> >
+                                </a>
+                                <?= ($k==$orden) ? icono_sentido($sentido) : ""; ?>
+                            </th><?php
+                        endif;
+                    endforeach;?>
+                    <th colspan="2" >Operaciones</th>
+                </thead>
+                <tbody><?php
+                    for($i = 0; $i < pg_num_rows($res); $i++):
+                        $fila = pg_fetch_assoc($res, $i);
+                    
+                    ?>
+                        
+                        <tr><?php
+                            foreach($columnas as $k => $v):
+                                if(isset($v['mostrar'])):
+                                    if(isset($v['formato'])) {
+                                        $valor = $fila[$v['formato']];
+                                    }
+                                    else {
+                                        $valor = $fila[$k];
+                                    } ?>
+                                    <td <?= alinear($v) ?>><?= $valor ?></td><?php
+                                endif;
+                            endforeach;?>
+                            <td colspan="2">
+                                <form action="index.php" method="get">
+                                    <input type="hidden" name="codigo" value="<?= trim($fila['codigo']) ?>" />
+                                    <label for="cantidad">Cantidad</label>
+                                    <input type="number" name="cantidad" value="0" min="0" max="<?= $fila['existencias']?>"/> 
+                                    <input type="submit" value="Añadir al carrito" />
+                                </form>
+                            </td>
+                        </tr><?php
+                    endfor; ?>
+                </tbody>
+            </table><?php
+        else: ?>
+            <h3>La búsqueda no ha dado ningún resultado.</h3><?php
+        endif;
+    }
---
+++ pedidos/index.php
@@ -0,0 +1,61 @@
+<?php session_start() ?>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Juguetes</title>
+    </head>
+    <body>
+        <?php
+        require 'auxiliar.php';
+        require '../comunes/auxiliar.php';
+
+        if (isset($_GET['codigo']) && isset($_GET['cantidad'])){
+            $producto = trim($_GET['codigo']);
+            $cantidad = trim($_GET['cantidad']);
+            if (isset($_SESSION['carrito'][$producto])) {
+                $_SESSION['carrito'][$producto] += $cantidad;
+            } else {
+                $carrito = array (
+                    $producto => $cantidad
+                );
+                $_SESSION['carrito'][$producto] = $cantidad;
+            }
+            var_dump($_SESSION['carrito']);
+        } 
+
+
+        $columnas = array(
+            'codigo' => array(
+                'bonito'   => 'Código',
+                'criterio' => 'codigo',
+                'exacto'   => TRUE,
+                'mostrar'  => TRUE
+            ),
+            'nombre' => array(
+                'bonito'   => 'Nombre',
+                'criterio' => 'nombre',
+                'mostrar'  => TRUE
+            ),
+            'descripcion' => array(
+                'bonito'   => 'Descripcion',
+                'criterio' => 'descripcion',
+                'mostrar'  => TRUE
+            ),
+            'precio_format' => array(
+                'bonito'    => 'Precio',
+                'criterio'  => 'precio_format',
+                'exacto'    => TRUE,
+                'mostrar'   => TRUE,
+                'align'     => 'right'
+            ),
+            'existencias' => array(
+                'bonito'   => 'Existencias',
+                'criterio' => 'existencias',
+                'exacto'   => TRUE,
+                'mostrar'  => TRUE
+            )
+        );
+
+        index1($columnas, 'v_articulos'); ?>
+    </body>
+</html>
