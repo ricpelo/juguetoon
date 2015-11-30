7160a3905557dca4bd6aabdf10c021995331f042
aurochipi <aurochipi@hotmail.com>
2015-11-30 15:30:27 +0100

    Cargar index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php session_start() ?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>JUGUETOON</title>
+		<meta charset="utf-8"/>
+	</head>
+	<body>
+		
+		<a href="usuarios"><button>Gestión de Usuarios</button></a>
+		<a href="articulos"><button>Gestión de Artículos</button></a>
+		<a href="pedidos"><button>Gestión de Pedidos</button></a>
+		
+	</body>
+</html>
