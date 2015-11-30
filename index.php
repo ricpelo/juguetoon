fd740cf93843ee5559b8d73d16fb88c927f351e9
aurochipi <aurochipi@hotmail.com>
2015-11-30 16:20:35 +0100

    Portal con logo

--- index.php
+++ index.php
@@ -7,10 +7,13 @@
 		<meta charset="utf-8"/>
 	</head>
 	<body>
-		
-		<a href="usuarios"><button>Gestión de Usuarios</button></a>
-		<a href="articulos"><button>Gestión de Artículos</button></a>
-		<a href="pedidos"><button>Gestión de Pedidos</button></a>
-		
+		<div align="center">
+			<img src="imagenes/juguetoon.jpg" style="margin:auto;">
+		</div>
+		<div align="center">
+			<a href="usuarios"><button>Gestión de Usuarios</button></a>
+			<a href="articulos"><button>Gestión de Artículos</button></a>
+			<a href="pedidos"><button>Gestión de Pedidos</button></a>
+		</div>
 	</body>
 </html>
