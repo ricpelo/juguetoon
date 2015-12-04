06722e86cec104f6b8aca1bb4bf0babeb0430198
Guillermo <jdkdejava@gmail.com>
2015-12-04 18:11:08 +0100

    boton de salir en no_admin.php

--- no_admin.php
+++ no_admin.php
@@ -8,5 +8,8 @@
         <!-- Solucion provisional para que los usuarios no administradores no vean el index.php de gestion -->
         <!-- Cuando se cree la gestion de compra, entonces se quitara este archivo -->
         <p><?= "Solucion al problema de la redireccion a los administradores" ?></p>
+        <a href="comunes/logout.php">
+            <button>Salir</button>
+        </a>
     </body>
 </html>
